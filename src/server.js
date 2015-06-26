52b699826fb7a923f58207e31a7035c6ee353bdf
Kevin Mees <kev.mees@gmail.com>
2015-06-27 00:43:11 +0200

    Use React to render server response

--- src/server.js
+++ src/server.js
@@ -1,11 +1,21 @@
 import Express from 'express'
+import React from 'react'
 
 const log = require('debug')('server')
-
 const app = new Express()
 
 app.use((req, res) => {
-  res.status(200).send('Hello World')
+  res.send(React.renderToString(
+    <html lang="en-us">
+      <head>
+        <meta charSet="utf-8"/>
+        <title>Universal React on Azure</title>
+      </head>
+      <body>
+        Hello World from React
+      </body>
+    </html>
+  ))
 })
 
 const port = process.env.port || 3000
